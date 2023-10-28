26bc5a445f112c499c9d6bbe4ee73eec95cd1a36
Rex2002 <frahm.benjamin@gmx.de>
2023-10-28 14:34:09 +0200

    feat: address points raised by @malteRichert in #3

--- src/main/java/de/dhbw/video/ShapeProcessor.java
+++ src/main/java/de/dhbw/video/ShapeProcessor.java
@@ -13,13 +13,13 @@ import static de.dhbw.statics.*;
 
 public class ShapeProcessor {
 
-    List<Shape> shapes;
+    private List<Shape> shapes;
     // consists of x_pos, y_pos, width, height, is_rect(0/1)
-    int[] playfieldInfo;
-    Mat[] playFieldBoundaries;
-    int frameHeight;
-    int frameWidth;
-    boolean[][] soundMatrix;
+    private final int[] playfieldInfo;
+    private final Mat[] playFieldBoundaries;
+    private int frameHeight;
+    private int frameWidth;
+    private boolean[][] soundMatrix;
     public ShapeProcessor(){
         playFieldBoundaries = new Mat[4];
         for(int i = 0; i < 4; i++) {
@@ -40,52 +40,49 @@ public class ShapeProcessor {
         // TODO > treat control markers
     }
 
-    public boolean[][] getSoundMatrix(){
-        return soundMatrix;
-    }
-
     private void detectPlayfield(){
         playfieldInfo[4] = 1;
         Shape[] squares = shapes.stream().filter(shape -> shape.getForm() == ShapeForm.SQUARE).toArray(Shape[]::new);
         if(squares.length < 4){
             playfieldInfo[4] = 0;
             System.err.println("Not enough squares for playfield");
+            return;
         }
-        else {
-            Shape[] corners = new Shape[]{squares[0], squares[1], squares[2], squares[3]};
-            for (Shape square : squares) {
-                // top left
-                if(Math.sqrt(square.pos[1] * square.pos[1] + square.pos[0] * square.pos[0]) < Math.sqrt(corners[0].pos[0] * corners[0].pos[0] + corners[0].pos[1] * corners[0].pos[1])){
-                    corners[0] = square;
-                }
-                // bottom right
-                if(Math.sqrt(square.pos[1] * square.pos[1] + square.pos[0] * square.pos[0]) > Math.sqrt(corners[2].pos[0] * corners[2].pos[0] + corners[2].pos[1] * corners[2].pos[1])){
-                    corners[2] = square;
-                }
-                // top right
-                if(Math.sqrt( square.pos[0] * square.pos[0] + (frameHeight - square.pos[1]) * (frameHeight - square.pos[1])) > Math.sqrt(corners[1].pos[0] * corners[1].pos[0] + (frameHeight - corners[1].pos[1]) * (frameHeight - corners[1].pos[1]))){
-                    corners[1] = square;
-                }
-                // bottom left
-                if(Math.sqrt((frameWidth - square.pos[0]) * (frameWidth - square.pos[0]) + square.pos[1] * square.pos[1]) > Math.sqrt((frameWidth - corners[3].pos[0]) * (frameWidth - corners[3].pos[0]) + corners[3].pos[1] * corners[3].pos[1])){
-                    corners[3] = square;
-                }
+        Shape[] corners = new Shape[]{squares[0], squares[1], squares[2], squares[3]};
+        // calculates the (Euclidean) distance of each square to each corner and checks if the respective square is the one closest to any corner.
+        // if the latter is the case, the found square is considered the new closest one for further comparisons
+        for (Shape square : squares) {
+            // top left
+            if(Math.sqrt(square.pos[1] * square.pos[1] + square.pos[0] * square.pos[0]) < Math.sqrt(corners[0].pos[0] * corners[0].pos[0] + corners[0].pos[1] * corners[0].pos[1])){
+                corners[0] = square;
             }
-            for(Shape c : corners) c.setType(ShapeType.FIELD_MARKER);
-            for(int i = 0; i < 4; i++){
-                playFieldBoundaries[i].put(0,0, corners[i].pos[0] - corners[(i+1) % 4].pos[0]);
-                playFieldBoundaries[i].put(1,0, corners[i].pos[1] - corners[(i+1) % 4].pos[1]);
+            // bottom right
+            if(Math.sqrt(square.pos[1] * square.pos[1] + square.pos[0] * square.pos[0]) > Math.sqrt(corners[2].pos[0] * corners[2].pos[0] + corners[2].pos[1] * corners[2].pos[1])){
+                corners[2] = square;
             }
-            playfieldInfo[0] = (corners[0].pos[0] + corners[3].pos[0]) / 2;
-            playfieldInfo[1] = (corners[0].pos[1] + corners[1].pos[1]) / 2;
-            playfieldInfo[2] = (corners[1].pos[0] + corners[2].pos[0]) / 2;
-            playfieldInfo[3] = (corners[2].pos[1] + corners[3].pos[1]) / 2;
-            playfieldInfo[4] = checkRectangularity() ? 1 : 0;
-            for(Shape s : shapes){
-                if(s.pos[0] > playfieldInfo[0] && s.pos[0] < playfieldInfo[0] + playfieldInfo[2]
-                        && s.pos[1] > playfieldInfo[1] && s.pos[1] < playfieldInfo[1] + playfieldInfo[3]){
-                    s.setType(ShapeType.SOUND_MARKER);
-                }
+            // top right
+            if(Math.sqrt( square.pos[0] * square.pos[0] + (frameHeight - square.pos[1]) * (frameHeight - square.pos[1])) > Math.sqrt(corners[1].pos[0] * corners[1].pos[0] + (frameHeight - corners[1].pos[1]) * (frameHeight - corners[1].pos[1]))){
+                corners[1] = square;
+            }
+            // bottom left
+            if(Math.sqrt((frameWidth - square.pos[0]) * (frameWidth - square.pos[0]) + square.pos[1] * square.pos[1]) > Math.sqrt((frameWidth - corners[3].pos[0]) * (frameWidth - corners[3].pos[0]) + corners[3].pos[1] * corners[3].pos[1])){
+                corners[3] = square;
+            }
+        }
+        for(Shape c : corners) c.setType(ShapeType.FIELD_MARKER);
+        for(int i = 0; i < 4; i++){
+            playFieldBoundaries[i].put(0,0, corners[i].pos[0] - corners[(i+1) % 4].pos[0]);
+            playFieldBoundaries[i].put(1,0, corners[i].pos[1] - corners[(i+1) % 4].pos[1]);
+        }
+        playfieldInfo[0] = (corners[0].pos[0] + corners[3].pos[0]) / 2;
+        playfieldInfo[1] = (corners[0].pos[1] + corners[1].pos[1]) / 2;
+        playfieldInfo[2] = (corners[1].pos[0] + corners[2].pos[0]) / 2;
+        playfieldInfo[3] = (corners[2].pos[1] + corners[3].pos[1]) / 2;
+        playfieldInfo[4] = checkRectangularity() ? 1 : 0;
+        for(Shape s : shapes) {
+            if (s.pos[0] > playfieldInfo[0] && s.pos[0] < playfieldInfo[0] + playfieldInfo[2]
+                    && s.pos[1] > playfieldInfo[1] && s.pos[1] < playfieldInfo[1] + playfieldInfo[3]) {
+                s.setType(ShapeType.SOUND_MARKER);
             }
         }
     }
@@ -116,4 +113,9 @@ public class ShapeProcessor {
         }
         return true;
     }
+
+    public boolean[][] getSoundMatrix(){
+        return soundMatrix;
+    }
+
 }
