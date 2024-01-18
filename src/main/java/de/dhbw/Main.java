27f0f2964f36ab892e18126a72b194fb61438496
Rex2002 <frahm.benjamin@gmx.de>
2024-01-18 15:14:19 +0100

    feat: add downsizing to 640x480

--- src/main/java/de/dhbw/Main.java
+++ src/main/java/de/dhbw/Main.java
@@ -32,7 +32,7 @@ public class Main {
         Setting setting;
         //UIMessage uiMessage = new UIMessage();
 
-        VideoInput videoIn = new VideoInput(0);
+        VideoInput videoIn = new VideoInput(2);
         MarkerRecognizer markerRecognizer = new MarkerRecognizer();
         ShapeProcessor shapeProcessor = new ShapeProcessor();
         PositionMarker positionMarker = new PositionMarker();
@@ -49,6 +49,7 @@ public class Main {
             // message independent code:
             clock.tick(System.currentTimeMillis());
             videoIn.grabImage(frame);
+            System.out.println(frame.width() + ", " + frame.height());
 
             markerRecognizer.setFrame(frame);
             markerRecognizer.detectShapes();
